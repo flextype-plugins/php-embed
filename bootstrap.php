7a7ef9dc23aca47ec697552dd1019d0b979cae48
Awilum <awilum@msn.com>
2020-07-30 21:11:54 +0300

    feat(core): update code base for new Flextype 0.9.9

--- bootstrap.php
+++ bootstrap.php
@@ -11,13 +11,13 @@
  * file that was distributed with this source code.
  */
 
-namespace Flextype;
+namespace Flextype\Plugin\PHPEmbed;
 
 use Thunder\Shortcode\ShortcodeFacade;
 use Thunder\Shortcode\Shortcode\ShortcodeInterface;
 
 // Shortcode: [php] echo "Hello World"; [/php]
-$flextype['shortcodes']->addHandler('php', function (ShortcodeInterface $s) {
+$flextype['shortcode']->add('php', function (ShortcodeInterface $s) {
     ob_start();
     eval($s->getContent());
     return ob_get_clean();
